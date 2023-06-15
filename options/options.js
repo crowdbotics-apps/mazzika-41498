89877260a3544b94b96560fc93500eb852dc0ca8
nour samir <nono.chess2010@gmail.com>
2023-06-15 00:03:32 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "mazzika-41498",
   url: "https://mazzika-41498.botics.co",
   api: "https://mazzika-41498.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
